1d35be95662f2eab8bd2be0ce7e7f8d9102e2a92
Ren <renxzb@gmail.com>
2024-07-02 19:49:35 +0900

    add test code

---
+++ test/subroutine.py
@@ -0,0 +1,8 @@
+class subroutine:
+    fun_name: str
+    input_list: list
+    def __init__(self, name, var_list) -> None:
+        self.fun_name = name
+        self.input_list = var_list
+
+    def gen_subroutine():
---
+++ test/variable.py
@@ -0,0 +1,4 @@
+class Varibale:
+    var_name: str
+    def __init__(self, name):
+        pass
